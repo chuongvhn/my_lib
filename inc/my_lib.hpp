61739bd3ff9aa0c39c4da520aa2695713cecb110
Chuong Vo Hoang (RBVH/ETA11) <chuong.vohoang@vn.bosch.com>
2021-08-29 16:15:38 +0700

    initial commit

---
+++ inc/my_lib.hpp
@@ -0,0 +1,8 @@
+#ifndef __MY_LIB_H__
+#define __MY_LIB_H__
+
+#include <string>
+
+std::string getIdByNum(const uint16_t f_num);
+
+#endif // __MY_LIB_H__
---
+++ src/my_lib.cpp
@@ -0,0 +1,5 @@
+#include "my_lib.hpp"
+
+std::string getIdByNum(const uint16_t f_num) {
+    return std::to_string(f_num);
+}
